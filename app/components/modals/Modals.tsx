97ee8f9fc1e5bc9cfc9956c647c04d8f2c440a16
ImanolTane05 <ImanolTane05@gmail.com>
2025-03-06 00:09:15 -0600

    14 log in modal (remember error codes)

--- app/components/modals/Modals.tsx
+++ app/components/modals/Modals.tsx
@@ -38,8 +38,12 @@ const Modal:React.FC<ModalProps> = ({
             <div className="relative w-[90%] md:w-[80%] lg:w-[700px] my-6 mx-auto h-auto">
                 <div className={`traslate duration-600 h-full ${showModal ? 'translate-y-0 opacity-100':'translate-y-full opacity-10'}`}>
                     <div className="w-full h-auto rounded-xl relative flex flex-col bg-white">
+
                         <header className="h-[60px] flex items-center p-6 rounded-t justify-center relative border-b">
-                            <div className="p-3 absolute left-3 hover:bg-gray-300 rounded-full cursor-pointer">
+                            <div 
+                            onClick={handClose}
+                            className="p-3 absolute left-3 hover:bg-gray-300 rounded-full cursor-pointer"
+                            >
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                                 </svg>
