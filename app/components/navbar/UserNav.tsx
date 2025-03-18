d039c9b883d7e0cf6a821863f4daef01c0d8c22d
ImanolTane05 <ImanolTane05@gmail.com>
2025-03-17 22:27:10 -0600

    21 implement authentication (log in ,sig in and log out )

--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -1,12 +1,19 @@
 'use client';
-import { useState } from "react";
+import React, { useState } from "react";
 
 import MenuLink from "./MenuLink";
+import LogoutButton from "../LogoutButton";
 
 import useLoginModal from "@/app/hooks/useLoginModal";
 import useSignupModal from "@/app/hooks/useSignupModal";
 
-const UserNav = () => {
+interface UserNavProps{
+    userId?: string | null;
+}
+
+const UserNav: React.FC<UserNavProps> = ({
+    userId
+}) => {
         const LoginModal = useLoginModal();
         const SignupModal = useSignupModal();
         const [isOpen, setIsOpen] = useState(false);
@@ -29,13 +36,17 @@ const UserNav = () => {
 
             {isOpen && (
                 <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col cursor-pointer">
-                    <MenuLink
+                    {userId ? (
+                        <LogoutButton />
+                    ) : (
+                        <>
+                        <MenuLink
                         label="Log in"
                         onClick={() =>{
                             setIsOpen(false);
                             LoginModal.open();
                         }}           
-                    />
+                        />
 
                         <MenuLink
                         label="Sign u"
@@ -43,7 +54,10 @@ const UserNav = () => {
                             setIsOpen(false);
                             SignupModal.open();
                         }}           
-                    />
+                        />
+                    </>
+                )}
+                    
                 </div>
                 )}
         </div>
--- app/components/properties/PropertyList.tsx
+++ app/components/properties/PropertyList.tsx
@@ -1,11 +1,39 @@
+'use client';
+
+
+import { useEffect,useState } from "react";
 import PropertyListItem from "./PropertyListItem";
+import apiServices from "@/app/services/apiServices";
+
+export type PropertyType ={
+    id:string;
+    title:String;
+    image_url:string;
+    price_per_night:number;
+}
 
 const PropertyList = () => {
+    const [properties,setProperties] = useState<PropertyType[]> ([]);
+    const getProperties = async () =>{
+       const tmpProperties = await apiServices.get('/api/properties/')
+
+       setProperties(tmpProperties.data);
+    };
+    useEffect (() =>{
+        getProperties();
+    },[]);
     return(
         <>
-            <PropertyListItem />
-            <PropertyListItem />
-            <PropertyListItem />
+            {properties.map((property)=>{
+                return(
+                    <PropertyListItem 
+                        key={property.id}
+                        property={property}
+                    />
+                )
+            })}
+            
+            
         </>
     )
 }
